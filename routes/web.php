62e2ad1d2faaa6fddfe2bc353ccf181e1026063a
mrXrobot26 <zezoelbessa10@gmail.com>
2025-01-30 20:52:38 +0200

    Add authentication middleware to routes for user and post management

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // User related routes
-Route::get('/', [UserController::class, 'showCorrentHomePage']);
+Route::get('/', [UserController::class, 'showCorrentHomePage'])->name('login');
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
 
 // Blog post related routes
-Route::post('/Create-Post', [PostController::class, 'showCreateForm']);
-Route::get('/Create-Post', [PostController::class, 'storeNewPost']);
-Route::get('/post/{post}', [PostController::class, 'viewPosts']); // Fixed route
+Route::post('/Create-Post', [PostController::class, 'showCreateForm'])->middleware('auth');
+Route::get('/Create-Post', [PostController::class, 'storeNewPost'])->middleware('auth');
+Route::get('/post/{post}', [PostController::class, 'viewPosts'])->middleware('auth');
